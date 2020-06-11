d5107b58b5f3cefb546eb52185783c901b74eeac
laksheenmendis <cresclux@yahoo.com>
2020-06-11 15:46:50 -0600

    Adding dependencies.

---
+++ src/main/java/sustain/kNN/preprocessing/SketchPreprocessor.java
@@ -0,0 +1,11 @@
+package sustain.kNN.preprocessing;
+
+/**
+ * Created by laksheenmendis on 6/11/20 at 3:39 PM
+ */
+public class SketchPreprocessor {
+
+    public static void main(String[] args) {
+
+    }
+}
